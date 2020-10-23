57065f0231d9acc2459212ea01b0031b642d7fb0
LAPTOP-UH0D04OH\MELİH <diksumelih@gmail.com>
2020-10-23 16:01:32 +0300

    my_first_branch: second alteration

--- src/mainGit.java
+++ src/mainGit.java
@@ -1,5 +1,5 @@
 public class mainGit {
     System.out.println("This is a git exercise");
     System.out.println("This is the first change");
-    System.out.println("This is the first branch alteration");
+    System.out.println("This is the first branch alteration 0.2");
 }
