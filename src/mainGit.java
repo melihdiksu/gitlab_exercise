40ce6a3e67a5a0bc76c820a8bda32fff43633e1c
LAPTOP-UH0D04OH\MELİH <diksumelih@gmail.com>
2020-10-23 15:59:23 +0300

    initial change on branch my_first_branch

--- src/mainGit.java
+++ src/mainGit.java
@@ -1,4 +1,5 @@
 public class mainGit {
     System.out.println("This is a git exercise");
     System.out.println("This is the first change");
+    System.out.println("This is the first branch alteration");
 }
